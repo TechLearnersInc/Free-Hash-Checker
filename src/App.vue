3e12e94d00946f65cfcd0478e2ac704fb61e0f2b
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2024-10-09 13:26:01 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid sm:grid-cols-12 grid-cols-12 sm:grid-rows-3 grid-rows-3">
+    <app-header-vue v-bind:git-repo-url="gitRepoUrl" v-bind:download-url="downloadUrl" />
+    <div class="bg-teal-500 col-span-12 row-span-1"><h1>Contents</h1></div>
+    <div class="bg-purple-500 col-span-12 row-span-1"><h1>Footer</h1></div>
+  </div>
+</template>
+
+<script>
+import AppHeaderVue from '@/components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeaderVue,
+  },
+  data() {
+    return {
+      gitRepoUrl: 'https://github.com/TechLearnersInc/Free-Hash-Checker.git',
+      downloadUrl: 'https://sourceforge.net/projects/free-hash-checker/files/v3.0/',
+    };
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-span-12 row-span-1">
+    <div>
+      <div class="flex justify-center items-center">
+        <img class="size-48 m-10" v-bind:src="appIcon" alt="app_icon" />
+      </div>
+      <div>
+        <div class="flex justify-center items-center">
+          <h1 class="font-lato text-5xl font-semibold text-gray-700 text-center">
+            Free Hash Checker
+          </h1>
+        </div>
+        <div class="flex justify-center items-center">
+          <h6 class="font-poppins text-xs font-semibold text-gray-700 text-center">v3.0</h6>
+        </div>
+        <div class="flex justify-center items-center">
+          <p class="font-poppins text-xl text-gray-500 text-center m-3">
+            A simple and elegant Free Hash Checker for desktop
+          </p>
+        </div>
+        <div class="flex justify-center items-center">
+          <a v-bind:href="downloadUrl"
+            ><button
+              type="button"
+              class="text-blue-400 hover:text-white border border-blue-400 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-xl px-5 py-2.5 text-center me-2 mb-2 mt-2"
+            >
+              Download
+            </button></a
+          >
+        </div>
+        <div class="flex justify-center items-center">
+          <a class="text-xs text-blue-500 mt-3" v-bind:href="gitRepoUrl">
+            Give this project a star on GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppIcon from '@/assets/images/icon.png';
+import AppHeaderIcon from '@/assets/images/header.png';
+
+export default {
+  name: 'AppHeader',
+  props: {
+    gitRepoUrl: {
+      type: String,
+      required: true,
+    },
+    downloadUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      appIcon: AppIcon,
+      appHeaderIcon: AppHeaderIcon,
+    };
+  },
+};
+</script>
